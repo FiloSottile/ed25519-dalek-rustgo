6ce7664672d09c8f3b54c81226751923b858bc63
Filippo Valsorda <hi@filippo.io>
2017-08-12 01:32:01 -0400

    Add docs

--- edwards25519/rustgo.go
+++ edwards25519/rustgo.go
@@ -1,5 +1,12 @@
 //go:binary-only-package
 
+// Package edwards25519 implements operations on an Edwards curve that is
+// isomorphic to curve25519.
+//
+// Crypto operations are implemented by calling directly into the Rust
+// library curve25519-dalek, without cgo.
+//
+// You should not actually be using this.
 package edwards25519
 
 import _ "unsafe"
@@ -10,4 +17,6 @@ import _ "unsafe"
 var scalar_base_mult uintptr
 var _scalar_base_mult = &scalar_base_mult
 
+// ScalarBaseMult multiplies the scalar in by the curve basepoint, and writes
+// the compressed Edwards representation of the resulting point to dst.
 func ScalarBaseMult(dst, in *[32]byte)
